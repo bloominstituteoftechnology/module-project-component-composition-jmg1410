1ab87627f738f020d548d8a89425a898a5cdc582
Jay Marmol <marmoljay@hotmail.com>
2024-07-12 19:32:46 -0400

    finished adding styled components

--- frontend/components/Figure.js
+++ frontend/components/Figure.js
@@ -1,10 +1,19 @@
 import React from "react"
+import styled from "styled-components"
+
+const StyledFigure = styled.figure`
+max-width: 400px;
+
+figcaption {
+font-style: italic;
+}
+`
 
 export default function Figure({imageURL, caption}) {
     return (
-      <figure>
+      <StyledFigure>
       <img src={imageURL}/>
       <figcaption>Awesome pic Taken on {caption}</figcaption>
-    </figure>
+    </StyledFigure>
     )
   }
